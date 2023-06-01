306b2e2d42b800069271a4fe41c61425e8261bfd
Akshay Singh <109530003+Akshay-Singh-273@users.noreply.github.com>
2023-06-01 20:34:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def predictRouteClient():
             for val in x:
                 output.append(pd.read_csv(out_path, skiprows=val-1, nrows = 1))
 
-            return Response("Prediction File created at !!!"  +str(path) +"and few of the predictions are "+ str(json.loads(output))
+            return Response("Prediction File created at !!!"  +str(path) +"and few of the predictions are "+ str(output))
 
             # predicting for dataset present in database
             path,json_predictions = pred.predictionFromModel()
